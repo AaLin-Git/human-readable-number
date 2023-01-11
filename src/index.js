de5f49fb986ce8bb408a7e0b056eed249dcc886f
AaLin-Git <aalin.box.it@gmail.com>
2023-01-11 16:41:14 +0100

    feat: make readable first 10 numbers

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ module.exports = function toReadable (number) {
     const numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     const tenToNineteen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     const dozens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    const numToStr = String(n);
+    const length = numToStr.length;
+    let result = '';
+
+    if (length === 1) {
+        return result = numbers[n];
+    }
 }
+

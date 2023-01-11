c5d70ba16e3627103650ef2a61efdbd83b43d56b
AaLin-Git <aalin.box.it@gmail.com>
2023-01-11 16:14:12 +0100

    init: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+    const numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    const tenToNineteen = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    const dozens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
 }
